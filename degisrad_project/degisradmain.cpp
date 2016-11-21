32482de2d93f4626de2631c52eacb7b0718c1576
Vera Blinova <vera.blinova.1996@gmail.com>
2016-11-22 00:11:45 +0700

    3

--- degisrad_project/degisradmain.cpp
+++ degisrad_project/degisradmain.cpp
@@ -1,6 +1,5 @@
 #include "degisrad.h"
 #include "ui_degisrad.h"
-#include <math.h>
 
 Degisrad::Degisrad(QWidget *parent) :
     QMainWindow(parent),
@@ -8,60 +7,53 @@ Degisrad::Degisrad(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    connect(this, SIGNAL(releasedError(QString)), this, SLOT(s_printError(QString)));
 }
 void Degisrad::s_printError(QString str)
 {
-    QMessageBox msg;
-    msg.critical(this, "Warning", str);
-};
+    QMessageBox::critical(this, "Warning", str);
+}
 
 void Degisrad::on_btn_do_clicked()
 {
-   connect( this,SIGNAL (releasedError(QString)),this,SLOT (s_printError(QString)));
+    bool ok;
 
-  // получаем градусную меру из line_deg, line_min и line_sec
-  double dDeg = ui->line_deg->text().toDouble(&ok);
-
-    if (ok==false)
-    {emit releasedError("It did not enter a number");
-     return;
+    // получаем градусную меру из line_deg, line_min и line_sec
+    double dDeg = ui->line_deg->text().toDouble(&ok);
+    if (!ok) {
+        emit releasedError("It did not enter a number");
+        return;
     };// error of trunslation
+    if (dDeg > 360) {
+        emit releasedError("Too much degracy ");
+        return;
+    };// error
 
-    if (dDeg > 360)
-    {emit releasedError("Too much degracy ");
-     return;
-     };// error
-
-  double dMin = ui->line_min->text().toDouble( &ok );
-
-    if (ok==false)
-    {emit releasedError("It did not enter a number");
-     return;
+    double dMin = ui->line_min->text().toDouble(&ok);
+    if (!ok) {
+        emit releasedError("It did not enter a number");
+        return;
     };// error of trunslation
-
-    if (dMin >= 60)
-    {emit releasedError("Too much minutes");
-     return;
+    if (dMin > 60) {
+        emit releasedError("Too much minutes");
+        return;
     }; // error
 
-  double dSec = ui->line_sec->text().toDouble(&ok);
-
-    if (ok==false)
-    { emit releasedError("It did not enter a number");
-      return;
+    double dSec = ui->line_sec->text().toDouble(&ok);
+    if (!ok) {
+        emit releasedError("It did not enter a number");
+        return;
     };// error of trunslation
-
-    if (dSec >= 60)
-    {emit releasedError("Too much seconds");
-     return;
+    if (dSec > 60) {
+        emit releasedError("Too much seconds");
+        return;
     };// error
 
 
-    if (dDeg + dMin + dSec > 360)
-    {emit releasedError("Too much degracy");
-     return;
+    if (dDeg + dMin + dSec > 360) {
+        emit releasedError("Too much degracy");
+        return;
     };// error
-
     double dRad = (dDeg + dMin / 60 + dSec / 3600) / 180 * M_PI;
     // выводим радианы  в line_rad
     ui->line_rad->setText(QString::number(dRad));
